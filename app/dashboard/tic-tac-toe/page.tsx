999ef2dce8b93e9bbf82694b114f9e3fcecd8ccb
rossnewby <ross.newby@penmanconsulting.com>
2025-11-17 14:28:08 +0000

    tic-tac-tow & next js chapter 1-5 tutorials

---
+++ app/dashboard/tic-tac-toe/page.tsx
@@ -0,0 +1,10 @@
+import '@/app/dashboard/tic-tac-toe/styles.css';
+import Game from "@/app/ui/tic-tac-toe/game";
+
+export default function Page() {
+  return <>
+    <div>
+      <Game />
+    </div>
+  </>;
+}
